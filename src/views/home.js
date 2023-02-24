71110644ddb5a5cf63c9d490ce266f3b3503f38b
frankthehr <frank.pierce@thehrcompany.ie>
2023-02-24 16:34:08 +0000

    Completed basic logic with job description prompt

--- src/views/home.js
+++ src/views/home.js
@@ -6,23 +6,64 @@ const Home = () => {
 
   const { random } = useContext(AppContext);
 
-  const [prompt, setPrompt] = useState('');
+  const [prompt, setPrompt] = useState(undefined);
+  const [answer, setAnswer] = useState(false);
+  const [loading, setLoading] = useState(undefined);
+
+  useEffect(() => {
+    if (prompt != null && prompt.trim() === "") {
+      setAnswer(undefined);
+    }
+  }, [prompt]);
 
   const sendPrompt = async (event) => {
     if (event.key !== "Enter") return;
 
-    console.log('Prompt:', prompt);
+    try {
+      console.log('Starting')
+      setLoading(true);
+
+      const request = {
+        method: "POST",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify({prompt})
+      }
+
+      const response = await fetch("http://localhost:5000/ask", request);
+
+      console.log('Reponse fetched');
+
+      if (!response.ok) {
+        console.log('Response not OK')
+        throw new Error("Something went wrong pal!")
+      }
+
+      const responseJSON = await response.json();
+
+      const responseCompletion = responseJSON.completion;
+
+      console.log(responseCompletion);
+
+      setAnswer(responseCompletion);
+      setLoading(false);
+
+    } catch (error) {
+      setLoading(false);
+      console.log('There was an error', error.message);
+    }
   }
 
   return (
     <div className={styles.home}>
       <div className={styles.form}>
-      <input
-        type="text"
-        placeholder="Ask me anything..."
-        onChange={(e) => setPrompt(e.target.value)}
-        onKeyDown={(e) => sendPrompt(e)}
-      />
+        <input
+          type="text"
+          placeholder="Ask me anything..."
+          onChange={(e) => setPrompt(e.target.value)}
+          onKeyDown={(e) => sendPrompt(e)}
+        />
+        <span>{ loading ? loading : " "}</span>
+        { answer && <span>{ answer }</span>}
       </div>
     </div>
   )
