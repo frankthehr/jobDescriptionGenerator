9f428d47338e6c95198c0d27d6feb64ff7238860
frankthehr <frank.pierce@thehrcompany.ie>
2023-03-09 10:34:57 +0000

    Removed unused pages, commented main logic, removed routes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './views/home';
 import NotFound from './views/notfound';
-import FrenchHome from './views/frenchhome';
-import NewHome from './views/newhome';
 
 export const AppContext = React.createContext();
 
@@ -15,9 +13,7 @@ function App() {
   return (
     <AppContext.Provider value={{ random, setRandom }}>
       <Routes>
-        <Route exact path='/' element={<NewHome />} />
-        <Route exact path='french' element={<FrenchHome />} />
-        <Route exact path='old' element={<Home />} />
+        <Route exact path='/' element={<Home />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </AppContext.Provider>
--- src/views/home.js
+++ src/views/home.js
@@ -6,46 +6,60 @@ const Home = () => {
 
   const { random } = useContext(AppContext);
 
-  const [prompt, setPrompt] = useState(undefined);
+  const [title, setTitle] = useState(undefined);
+  const [years, setYears] = useState(undefined);
+  const [location, setLocation] = useState(undefined);
+  const [email, setEmail] = useState(undefined);
   const [answer, setAnswer] = useState(false);
   const [loading, setLoading] = useState(undefined);
+  const [buttonURL, setButtonURL] = useState('');
 
-  useEffect(() => {
-    if (prompt != null && prompt.trim() === "") setAnswer(undefined);
-  }, [prompt]);
-
-  const sendPrompt = async (event) => {
-    if (event.key !== "Enter") return;
+  // Function to create POST request, call backend and create PDF from response
+  const sendPrompt = async () => {
 
     try {
       console.log('Starting')
       setLoading(true);
 
+      // Create POST request with body containing state data
       const request = {
         method: "POST",
         headers: { "Content-Type": "application/json"},
-        body: JSON.stringify({prompt})
-      }
+        body: JSON.stringify({
+          title,
+          years,
+          location,
+          email
+        })
+      };
 
+      // Call backend with above request
       const response = await fetch("http://localhost:5000/jobdescription", request);
 
       console.log('Reponse fetched');
 
       console.log(response);
 
+      // Throw error if request fails
       if (!response.ok) {
         console.log('Response not OK')
         throw new Error("Something went wrong pal!")
       }
 
-      const responseJSON = await response.json();
+      // Reads response stream and returns a promise which resolves with an Array Buffer (An array buffer is a low-level data structure in programming that represents a fixed-length, contiguous memory area of raw binary data. It is a type of buffer that is commonly used for handling large amounts of data efficiently, especially when dealing with multimedia data such as images, audio, and video.)
+      const responseBuffer = await response.arrayBuffer();
 
-      const responseCompletion = responseJSON.completion;
+      // Create blob with PDF MIME type from response array buffer. A blob (binary short object) is a data type used to represent large binary data like images or files.
+      const pdfBlob = new Blob([responseBuffer], {type: 'application/pdf'});
 
-      console.log(responseCompletion);
+      // Create URL to reference PDF blob
+      const href = URL.createObjectURL(pdfBlob);
 
-      setAnswer(responseCompletion);
+      // Set buttonURL to PDF blob URL
+      setButtonURL(href);
+
       setLoading(false);
+      setAnswer(true);
 
     } catch (error) {
       setLoading(false);
@@ -57,25 +71,56 @@ const Home = () => {
     <div className={styles.home}>
       <div className={styles.form}>
         <h1>Job Description Generator</h1>
-        <h5>Write the job title you need below and press Enter</h5>
-        <input
-          type="text"
-          placeholder="Job Descripton"
-          onChange={(e) => setPrompt(e.target.value)}
-          onKeyDown={(e) => sendPrompt(e)}
-        />
-        { loading &&
-          <div className={styles.loading}>
-            <span>Your job description is being generated</span>
-            <svg width="24" height="24" viewBox="0 0 24 24">
-              <path fill="none" stroke="currentColor" strokeDasharray="15" strokeDashoffset="15" strokeLinecap="round" strokeWidth="2" d="M12 3C16.9706 3 21 7.02944 21 12">
-                <animate fill="freeze" attributeName="stroke-dashoffset" dur="0.3s" values="15;0"/>
-                <animateTransform attributeName="transform" dur="1.5s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12"/>
-              </path>
-            </svg>
-          </div>
+
+        <div className={styles.title}>
+          <label htmlFor="jd-title">Job title</label>
+          <input
+            type="text"
+            id="jd-title"
+            placeholder="Head Chef"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+
+        <div className={styles.years}>
+          <label htmlFor="jd-number">Years of Experience</label>
+          <input
+            type="number"
+            id="jd-number"
+            placeholder="3"
+            onChange={(e) => setYears(e.target.value)}
+          />
+        </div>
+
+        <div className={styles.location}>
+          <label htmlFor="jd-location">Location of the role</label>
+          <input
+            type="text"
+            id="jd-location"
+            placeholder="Dublin, Ireland"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </div>
+
+        <div className={styles.email}>
+          <label htmlFor="jd-email">Contact Email</label>
+          <input
+            type="text"
+            id="jd-email"
+            placeholder="youremail@email.com"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <button onClick={sendPrompt}>
+          { loading ? 'Creating...' : 'Create Job Description' }
+        </button>
+
+        { answer &&
+          <a href={buttonURL} download="yay.pdf">
+            Open PDF
+          </a>
         }
-        { answer && <span>{ answer }</span>}
       </div>
     </div>
   )
